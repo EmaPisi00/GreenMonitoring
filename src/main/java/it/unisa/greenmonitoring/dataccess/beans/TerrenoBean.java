ab15a789033e57e5c043ecaf4f5f1ca84e612404
“nicolamoothreemodugno” <nmodugno48@gmail.com>
2023-01-14 20:52:14 +0100

    Aggiunto il costruttore di TerrenoBean in package it.unisa.greenmonitoring.dataccess.beans;

--- src/main/java/it/unisa/greenmonitoring/dataccess/beans/TerrenoBean.java
+++ src/main/java/it/unisa/greenmonitoring/dataccess/beans/TerrenoBean.java
@@ -6,4 +6,19 @@ public class TerrenoBean {
     private String superficie;
     private String immagine;
 
+    /**
+     * Questo metodo crea un terreno bean a partire da i seguenti parametri.
+     * @param id
+     * @param locazione
+     * @param superficie
+     * @param immagine
+     * @return un'istanza di TerrenoBean
+     */
+    public TerrenoBean(String id, String locazione, String superficie, String immagine) {
+        this.id = id;
+        this.locazione = locazione;
+        this.superficie = superficie;
+        this.immagine = immagine;
+    }
+
 }
