23ad95deb6e609b216cc88ff1f38640ec40e207a
EmaPisi00 <emanuelepisaturo@hotmail.it>
2023-01-14 12:58:23 +0100

    Add ConnectionPool class and upgrade pom.xml

---
+++ src/main/java/it/unisa/greenmonitoring/dataccess/dao/ConnectionPool.java
@@ -0,0 +1,90 @@
+package it.unisa.greenmonitoring.dataccess.dao;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Connessione DB.
+ */
+public final class ConnectionPool {
+
+    /**
+     * Costruttore.
+     */
+    private ConnectionPool() {
+
+    }
+    /**
+     * Dichiaro List<Connection>.
+     */
+    private static List<Connection> freecon;
+
+    static {
+        freecon = new LinkedList<Connection>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Creazione metodo DB Connection.
+     * @return connection
+     * @throws SQLException
+     */
+   private static synchronized Connection createDBConnection() throws SQLException {
+
+
+                // non serve  Class.forName("com.mysql.jdbc.Driver");
+                String url = "jdbc:mysql://localhost:3306/greenmonitoringdb";
+                String user = "root";
+                String pwd = "311000";
+                Connection con = DriverManager.getConnection(url, user, pwd);
+
+                con.setAutoCommit(false);
+                System.out.println("Connessione Eseguita!");
+                return con;
+        }
+
+    /**
+     * Metodo che richiama la connessione al DB.
+      * @return connection
+     * @throws SQLException
+     */
+    public static synchronized Connection getConnection() throws SQLException {
+
+        Connection con;
+
+        if (!freecon.isEmpty()) {
+            con = (Connection) freecon.get(0);
+            freecon.remove(0);
+
+            try {
+                if (con.isClosed()) {
+                    con = getConnection();
+                }
+            } catch (SQLException e) {
+                con.close();
+                con = getConnection();
+            }
+        } else {
+            con = createDBConnection();
+        }
+        return con;
+
+    }
+
+    /**
+     * Connessione.
+     * @param c
+     * @throws SQLException
+     */
+    public static synchronized void relaseConnection(Connection c) throws SQLException {
+        if (c != null) {
+            freecon.add(c);
+        }
+    }
+
+
+}
