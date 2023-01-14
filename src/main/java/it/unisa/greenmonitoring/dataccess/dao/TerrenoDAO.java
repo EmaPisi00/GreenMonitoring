f5d1c3324a91f4473062591fbd1d79072ab38a0f
“nicolamoothreemodugno” <nmodugno48@gmail.com>
2023-01-14 20:31:51 +0100

    Aggiunta retrieve() a TerrenoDAO in it.unisa.greenmonitoring.dataccess.dao

--- src/main/java/it/unisa/greenmonitoring/dataccess/dao/TerrenoDAO.java
+++ src/main/java/it/unisa/greenmonitoring/dataccess/dao/TerrenoDAO.java
@@ -1,9 +1,12 @@
 package it.unisa.greenmonitoring.dataccess.dao;
 
+import it.unisa.greenmonitoring.dataccess.beans.TerrenoBean;
+
 import java.sql.SQLException;
 
 public interface TerrenoDAO {
     public void create(TerrenoBean t) throws SQLException;
+    public TerrenoBean retrieve(String id_terreno) throws SQLException;
 
 
 }
