e38cb40bb4d7251da98e6338ffe9877f653ff6ff
“nicolamoothreemodugno” <nmodugno48@gmail.com>
2023-01-14 20:51:06 +0100

    Aggiunta documentazione a delete(), retrieve(), create() e update() di TerrenoDAO in it.unisa.greenmonitoring.dataccess.dao

--- src/main/java/it/unisa/greenmonitoring/dataccess/beans/TerrenoBean.java
+++ src/main/java/it/unisa/greenmonitoring/dataccess/beans/TerrenoBean.java
@@ -1,5 +1,9 @@
 package it.unisa.greenmonitoring.dataccess.beans;
 
 public class TerrenoBean {
+    private String id;
+    private String locazione;
+    private String superficie;
+    private String immagine;
 
 }
--- src/main/java/it/unisa/greenmonitoring/dataccess/dao/TerrenoDAO.java
+++ src/main/java/it/unisa/greenmonitoring/dataccess/dao/TerrenoDAO.java
@@ -6,8 +6,33 @@ import java.sql.SQLException;
 
 
 public interface TerrenoDAO {
+    /**
+     * Questo metodo crea un record nella tabella TERRENO.
+     * @param t
+     * @throws SQLException
+     * @return void
+     */
     public void create(TerrenoBean t) throws SQLException;
+    /**
+     * Questo metodo restituisce una istanza TerrenoBean costruita dai dati nel record con l'id passato come parametro.
+     * @param id_terreno
+     * @throws SQLException
+     * @return TerrenoBean
+     */
     public TerrenoBean retrieve(String id_terreno) throws SQLException;
+
+    /**
+     * Questo metodo aggiorna un record nella tabella TERRENO.
+     * @param id_terreno
+     * @throws SQLException
+     * @return void
+     */
     public void update(String id_terreno) throws SQLException;
+    /**
+     * Questo metodo distrugge un record nella tabella TERRENO.
+     * @param id_terreno
+     * @throws SQLException
+     * @return void
+     */
     public void delete(String id_terreno) throws SQLException;
 }
