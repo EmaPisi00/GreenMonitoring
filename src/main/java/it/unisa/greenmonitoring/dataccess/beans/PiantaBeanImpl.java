1c227c786363dd3e4e9966954afc25aa81fada3e
“nicolamoothreemodugno” <nmodugno48@gmail.com>
2023-01-14 12:56:46 +0100

    Aggiunti campi in PiantaBean in package it.unisa.greenmonitoring.dataccess.beans;

--- src/main/java/it/unisa/greenmonitoring/dataccess/beans/PiantaBeanImpl.java
+++ src/main/java/it/unisa/greenmonitoring/dataccess/beans/PiantaBeanImpl.java
@@ -1,5 +1,17 @@
 package it.unisa.greenmonitoring.dataccess.beans;
 
 public class PiantaBeanImpl implements PiantaBean{
+    private String id;
+    private String nome;
+    private int temperatura_min;
 
+    private int temperatura_max;
+
+    private int ph_max;
+
+    private String descrizione;
+
+    private int umidita_max;
+
+    private int umidita_min;
 }
