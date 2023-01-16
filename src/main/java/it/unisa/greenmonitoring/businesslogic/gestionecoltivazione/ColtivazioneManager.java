01c12d6de7382f0330c7443ec4447339abf1938d
manuelstefanile <m.stefanile3@studenti.unisa.it>
2023-01-16 12:06:46 +0100

    I created the ColtivazioneManager interface

--- src/main/java/it/unisa/greenmonitoring/businesslogic/gestionecoltivazione/ColtivazioneManager.java
+++ src/main/java/it/unisa/greenmonitoring/businesslogic/gestionecoltivazione/ColtivazioneManager.java
@@ -1,4 +1,36 @@
 package it.unisa.greenmonitoring.businesslogic.gestionecoltivazione;
 
-public class ColtivazioneManager {
+import it.unisa.greenmonitoring.dataccess.beans.TerrenoBean;
+
+import java.sql.SQLException;
+
+public class ColtivazioneManager implements ColtivazioneManagerInterface {
+
+    @Override
+    public TerrenoBean createTerreno(TerrenoBean t, String email) throws SQLException {
+        if(!(t.getSuperficie().matches("[a-zA-Z]]"))){
+
+        }
+        else {
+            System.out.println("la superfice non può contenere lettere, ma solo numeri");
+        }
+        /*devo ancora implementare altri controlli
+         */
+        return null;
+    }
+
+    @Override
+    public TerrenoBean retrieveTerreno(String id_terreno) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void updateTerreno(String id_terreno) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteTerreno(String id_terreno) throws SQLException {
+
+    }
 }
