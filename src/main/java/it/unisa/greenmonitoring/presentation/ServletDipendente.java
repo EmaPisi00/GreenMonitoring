616e2fbc566b35416e4dc9c0e3c593269d2dba43
EmaPisi00 <emanuelepisaturo@hotmail.it>
2023-01-17 20:27:39 +0100

    Add ServletDipendente

--- src/main/java/it/unisa/greenmonitoring/presentation/ServletDipendente.java
+++ src/main/java/it/unisa/greenmonitoring/presentation/ServletDipendente.java
@@ -1,7 +1,7 @@
 package it.unisa.greenmonitoring.presentation;
 
 import it.unisa.greenmonitoring.businesslogic.autenticazione.AutenticazioneManager;
-import it.unisa.greenmonitoring.dataccess.beans.AziendaBean;
+import it.unisa.greenmonitoring.dataccess.beans.DipendenteBean;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -9,8 +9,9 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.SQLException;
 
-@WebServlet(name = "ServletAzienda", value = "/ServletAzienda")
-public class ServletAzienda extends HttpServlet {
+@WebServlet(name = "ServletDipendente", value = "/ServletDipendente")
+public class ServletDipendente extends HttpServlet {
+
 
     /**
      * Dichiaro la variabile privata AziendaManager.
@@ -27,45 +28,48 @@ public class ServletAzienda extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String nome = request.getParameter("inputNome");
+        String cognome = request.getParameter("inputCognome");
         String email = request.getParameter("inputEmail");
         String confermEmail = request.getParameter("confermaInputEmail");
         String password = request.getParameter("inputPassword");
         String confermPassword = request.getParameter("confermaInputPassword");
         String indirizzo = request.getParameter("inputIndirizzo");
-        String telefono = request.getParameter("inputTelefono");
         String citta = request.getParameter("inputCitta");
         String provincia = request.getParameter("inputProvincia");
-        String nome_azienda = request.getParameter("inputAzienda");
-        String partita_iva = request.getParameter("inputPartitaIva");
+        String telefono = request.getParameter("inputTelefono");
+        String azienda = request.getParameter("inputAzienda");
 
         if (!(password.equals(confermPassword)) || !(email.equals(confermEmail))) {
-            System.out.println("\nErrore ");
-            response.sendRedirect("RegistrazioneAzienda.jsp");
+            System.out.println("\nErrore email o password diverse ");
+            response.sendRedirect("Dipendente.jsp");
         } else {
 
-            AziendaBean aziendaBean = new AziendaBean();
+            DipendenteBean dipendenteBean = new DipendenteBean();
 
-            aziendaBean.setEmail(email);
-            aziendaBean.setPassword(password);
-            aziendaBean.setIndirizzo(indirizzo);
-            aziendaBean.setTelefono(telefono);
-            aziendaBean.setProvincia(provincia);
-            aziendaBean.setCitta(citta);
-            aziendaBean.setNome_azienda(nome_azienda);
-            aziendaBean.setPartita_iva(partita_iva);
+            dipendenteBean.setAzienda(azienda);
+            dipendenteBean.setCognome(cognome);
+            dipendenteBean.setNome(nome);
+            dipendenteBean.setCitta(citta);
+            dipendenteBean.setPassword(password);
+            dipendenteBean.setEmail(email);
+            dipendenteBean.setTelefono(telefono);
+            dipendenteBean.setProvincia(provincia);
+            dipendenteBean.setIndirizzo(indirizzo);
 
             try {
-                aziendaManager.registraAzienda(aziendaBean);
+                aziendaManager.registraDipendente(dipendenteBean);
                 response.sendRedirect("index.jsp");
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
 
+            }
+
         }
-    }
 
     /**
-     * Metodo doPost.
+     * Metodo get.
      * @param request
      * @param response
      * @throws ServletException
