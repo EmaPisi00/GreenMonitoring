316890dc5a83ec36eb8d8858297d7844dd1ef2a0
t3ch3 <teche_negative@yahoo.com>
2023-01-18 19:27:25 +0100

    Added "doRetrieve" method to AziendaDAOImpl. Modified LoginServlet to retrieve the current user informations if the logged user is "Azienda".

--- src/main/java/it/unisa/greenmonitoring/presentation/LoginServlet.java
+++ src/main/java/it/unisa/greenmonitoring/presentation/LoginServlet.java
@@ -1,6 +1,11 @@
 package it.unisa.greenmonitoring.presentation;
 
 import it.unisa.greenmonitoring.businesslogic.autenticazione.LoginManager;
+import it.unisa.greenmonitoring.dataccess.beans.AziendaBean;
+import it.unisa.greenmonitoring.dataccess.beans.DipendenteBean;
+import it.unisa.greenmonitoring.dataccess.beans.UtenteBean;
+import it.unisa.greenmonitoring.dataccess.dao.AziendaDAOImpl;
+import it.unisa.greenmonitoring.dataccess.dao.DipendenteDAOImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 @WebServlet(name = "LoginServlet", value = "/LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -44,10 +50,22 @@ public class LoginServlet extends HttpServlet {
         try {
             String checkRole = lm.CheckData(email, password);
             if (checkRole.matches("azienda")) {
-                sessione.setAttribute("currentUserSession", email);
-                response.sendRedirect("Terreni.jsp");
+                UtenteBean user = new AziendaBean();
+                user.setEmail(request.getParameter("email"));
+                user.setPassword(request.getParameter("password"));
+                user = AziendaDAOImpl.doRetrieve(user);
+                sessione.setAttribute("currentUserSession", user);
+                //ciclo for per la verifica del corretto retrieve delle informazioni del user corrente
+                for (String s : Arrays.asList(user.getEmail(), user.getPassword(), user.getTelefono(), user.getCitta(), user.getProvincia(), user.getIndirizzo())) {
+                    System.out.println(s);
+                }
+                response.sendRedirect("Dipendente.jsp");
             } else if (checkRole.matches("dipendente")) {
-                sessione.setAttribute("currentUserSession", email);
+                UtenteBean user = new DipendenteBean();
+                user.setEmail(request.getParameter("email"));
+                user.setPassword(request.getParameter("password"));
+                //user = DipendenteDAOImpl.doRetrieve(user);
+                sessione.setAttribute("currentUserSession", user);
                 response.sendRedirect("Dipendente.jsp");
             } else {
                 response.sendRedirect("index.jsp?error=true");
