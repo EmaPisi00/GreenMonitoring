6209a91b65a1d32c0e84afaea6e9df369afe2393
manuelstefanile <m.stefanile3@studenti.unisa.it>
2023-01-20 19:03:25 +0100

    i have added controlloDatiAggiorna for update of a Utente

--- src/main/java/it/unisa/greenmonitoring/businesslogic/autenticazione/UtenteManager.java
+++ src/main/java/it/unisa/greenmonitoring/businesslogic/autenticazione/UtenteManager.java
@@ -1,12 +1,13 @@
 package it.unisa.greenmonitoring.businesslogic.autenticazione;
 
-
+import it.unisa.greenmonitoring.dataccess.beans.AziendaBean;
 import it.unisa.greenmonitoring.dataccess.beans.DipendenteBean;
 
+import it.unisa.greenmonitoring.dataccess.beans.UtenteBean;
+import it.unisa.greenmonitoring.dataccess.dao.AziendaDAOImpl;
 
 import it.unisa.greenmonitoring.dataccess.dao.DipendenteDAO;
-
-
+import it.unisa.greenmonitoring.dataccess.dao.DipendenteDAOImpl;
 
 
 import java.sql.SQLException;
@@ -24,13 +25,46 @@ public class UtenteManager {
      * @return dipendente
      * @throws SQLException
      */
-    public boolean rimuoviDipendente(String email) throws SQLException {
+    public List<DipendenteBean> retrieveAll(String email) throws SQLException {
+        return null;
+    }
+    /**
+     * Metodo che permette la rimozione di una associazione ad un'azienda.
+     * @param email
+     * @return dipendente
+     * @throws SQLException
+     */
+    /*public boolean rimuoviDipendente(String email) throws SQLException {
         DipendenteBean dipendente = (DipendenteBean) dipendenteDAO.retrieveAllForKey(email);
         if (dipendente != null) {
             dipendenteDAO.updateAziendaToNull(email);
             return true;
         }
         return false;
+    }*/
+
+    /**
+     * Metodo che permette di aggiornare un dipendente.
+     * @param utenteNuovo
+     * @param vecchioUtente
+     * @throws SQLException
+     */
+    public void controllaDatiAggiorna(UtenteBean utenteNuovo, UtenteBean vecchioUtente) throws SQLException {
+
+        if (utenteNuovo instanceof AziendaBean) {
+            AziendaDAOImpl aziendaDao = new AziendaDAOImpl();
+            if (((AziendaBean) utenteNuovo).compareTo((AziendaBean) vecchioUtente) != 0) {
+                aziendaDao.update((AziendaBean) utenteNuovo, vecchioUtente.getEmail());
+            }
+
+        } else if (utenteNuovo instanceof DipendenteBean) {
+            DipendenteDAOImpl dipendenteDao = new DipendenteDAOImpl();
+            if (((DipendenteBean) utenteNuovo).compareTo((DipendenteBean) vecchioUtente) != 0) {
+                System.out.println("ci sono modifiche da aggiornare");
+                dipendenteDao.update((DipendenteBean) utenteNuovo, vecchioUtente.getEmail());
+            }
+
+        }
     }
 }
 
