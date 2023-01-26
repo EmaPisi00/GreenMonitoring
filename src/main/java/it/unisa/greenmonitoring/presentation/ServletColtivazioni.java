d3b01aeaf54e8c0dd4867108f168e140aaa216bb
“nicolamoothreemodugno” <nmodugno48@gmail.com>
2023-01-26 19:25:49 +0100

    Fixed ServletColtivazioni

--- src/main/java/it/unisa/greenmonitoring/presentation/ServletColtivazioni.java
+++ src/main/java/it/unisa/greenmonitoring/presentation/ServletColtivazioni.java
@@ -1,6 +1,12 @@
 package it.unisa.greenmonitoring.presentation;
 
+import it.unisa.greenmonitoring.businesslogic.gestionecoltivazione.ColtivazioneManager;
+import it.unisa.greenmonitoring.businesslogic.gestionecoltivazione.PiantaManager;
+import it.unisa.greenmonitoring.dataccess.beans.ColtivazioneBean;
+import it.unisa.greenmonitoring.dataccess.beans.PiantaBean;
 import it.unisa.greenmonitoring.dataccess.beans.UtenteBean;
+import it.unisa.greenmonitoring.dataccess.dao.PiantaDAO;
+import it.unisa.greenmonitoring.dataccess.dao.PiantaDAOImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +14,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "ServletColtivazioni", value = "/ServletColtivazioni")
 public class ServletColtivazioni extends HttpServlet {
@@ -36,21 +45,34 @@ public class ServletColtivazioni extends HttpServlet {
                 response.sendError(401);
             } else {
                 response.sendError(501);
-                /*
+                String utente = (String) request.getSession().getAttribute("currentUserSession");
+                String nomepianta = request.getParameter("nomepianta");
+                PiantaBean pb = new PiantaBean( utente,
+                        nomepianta,
+                        nomepianta,
+                        "0", "9", "0", "9");
                 PiantaManager pm = new PiantaManager();
-                PiantaBean pb = new PiantaBean( (String) request.getSession().getAttribute("currentUserSession"),
-                        request.getParameter("nomepianta"),
-                        request.getParameter("nomepianta"),
-                        0, 9, 0, 9);
+                PiantaDAOImpl pdao = new PiantaDAOImpl();
                 try {
                     pm.CreaPiantaManager(pb);
+                    // -1 perchè sarebbe ottimo avere un retreive pianta nel manager, in modo da averne l'id.
+                    int id = 0;
+                    List<PiantaBean> ap = pdao.RetriveAllPianta();
+                    for (int i = 0; i < ap.size(); i++) {
+                        if (ap.get(i).getNome().equals(nomepianta)) {
+                            if (ap.get(i).getAzienda() == null) {
+                                id = ap.get(i).getId();
+                                break;
+                            }
+                        }
+                    }
+                    ColtivazioneBean cb = new ColtivazioneBean( id, Integer.parseInt(request.getParameter("terreno")), Byte.parseByte("0"));
+                    ColtivazioneManager cm = new ColtivazioneManager();
+                    cm.avvioColtivazione(cb);
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
-                //-1 perchè sarebbe ottimo avere un retreive pianta nel manager, in modo da averne l'id.
-                ColtivazioneBean cb = new ColtivazioneBean(-1, Integer.parseInt(request.getParameter("terreno")), Byte.parseByte("0"));
-                ColtivazioneManager cm = new ColtivazioneManager();
-                cm.avvioColtivazione(cb);*/
+
             }
         }
     }
