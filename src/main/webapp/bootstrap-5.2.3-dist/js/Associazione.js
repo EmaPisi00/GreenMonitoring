ba9ef7945020084ae95c6e3688450b19b322dd29
Frank182000 <francymandy1820@gmail.com>
2023-01-29 11:32:12 +0100

    Add Associazione.js

---
+++ src/main/webapp/bootstrap-5.2.3-dist/js/Associazione.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $("#alrt").hide();
+
+    $("#summit").click(function(){
+        $("#associa_dipendente").submit();
+    });
+
+    $("#showModal").click(function() {
+                $("#alrt").fadeOut();
+                $('#Modal').modal('toggle');
+        });
+
+    $("#closeModal").click(function(){
+        $('#Modal').modal('hide')
+    });
+});
