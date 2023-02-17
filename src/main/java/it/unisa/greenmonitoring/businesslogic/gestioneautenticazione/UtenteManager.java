f606c6c3c150f080a032377a6ad35565d94424be
manuelstefanile <m.stefanile3@studenti.unisa.it>
2023-02-17 18:36:16 +0100

    Fixed UtenteManager,SensoreDAOImp ServletDipendente

--- src/main/java/it/unisa/greenmonitoring/businesslogic/gestioneautenticazione/UtenteManager.java
+++ src/main/java/it/unisa/greenmonitoring/businesslogic/gestioneautenticazione/UtenteManager.java
@@ -2,11 +2,14 @@ package it.unisa.greenmonitoring.businesslogic.gestioneautenticazione;
 
 import it.unisa.greenmonitoring.dataccess.beans.AziendaBean;
 import it.unisa.greenmonitoring.dataccess.beans.DipendenteBean;
+
 import it.unisa.greenmonitoring.dataccess.beans.UtenteBean;
 import it.unisa.greenmonitoring.dataccess.dao.AziendaDAO;
 import it.unisa.greenmonitoring.dataccess.dao.AziendaDAOImpl;
+
 import it.unisa.greenmonitoring.dataccess.dao.DipendenteDAO;
 import it.unisa.greenmonitoring.dataccess.dao.DipendenteDAOImpl;
+
 
 import java.sql.SQLException;
 
@@ -28,6 +31,7 @@ public class UtenteManager {
         this.aziendaDAO = new AziendaDAOImpl();
     }
 
+
     /**
      * Metodo che permette l'associazione di un dipendente ad un'azienda.
      * @param user
@@ -36,15 +40,10 @@ public class UtenteManager {
      */
     public boolean associazioneDipendente(DipendenteBean user, String codiceAzienda) throws SQLException {
         AziendaBean azienda;
-        if (user.getAzienda() != null) { //controlla se l'utente è già associato
-            return false;
-        } else if (!(codiceAzienda.matches("^\\w{8}$"))) { //stringa di 8 char, es: ASDdd234
-            return false;
-        }
         try {
             azienda = aziendaDAO.retrieveByCode(codiceAzienda);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return false;
         }
         if (azienda != null) {
             user.setAzienda(azienda.getEmail());
@@ -52,7 +51,7 @@ public class UtenteManager {
                 dipendenteDAO.doUpdate(user);
                 return true;
             } catch (SQLException e) {
-                throw new RuntimeException(e);
+                return false;
             }
         }
         return false;
